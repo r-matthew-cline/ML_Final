66c890570f69110c31371bf4507d3f83d025fc39
Matthew Cline <cline.48@wright.edu>
2017-11-17 12:06:00 -0500

    Added the code to do the SVM task. Updated the gitignore to ignore locked files that add a character to the end of the extension.

--- main.py
+++ main.py
@@ -21,6 +21,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.metrics import mean_squared_error
+
+np.set_printoptions(threshold=np.nan)
 
 
 ####### PREPROCESSING FUNCTIONS #######
@@ -55,3 +59,50 @@ def scaleFeatures(data, exceptions):
 data = pd.read_csv('train.csv') # Assumes that the data is in the PWD
 data = shuffleData(data)
 train, val, test = splitData(data)
+
+trainFeatures = train.iloc[38:1339,2:]
+trainLabels = train.iloc[38:1339:,1]
+
+valFeatures = val.iloc[:,2:]
+valLabels = val.iloc[:,1]
+
+testFeatures = test.iloc[:,2:]
+testLabels = test.iloc[:,1]
+
+
+
+####### SUPPORT VECTOR MACHINE ########
+
+clf = svm.SVC()
+clf.fit(trainFeatures, trainLabels)
+predictions = clf.predict(testFeatures)
+print(predictions)
+
+correct = 0
+for prediction, label in zip(predictions, testLabels):
+    if prediction == label:
+        correct += 1
+
+accuracy = correct / len(predictions) * 100
+print("Accuracy: ", accuracy, "%\n")
+
+
+
+
+
+
+
+####### LOGISTIC REGRESSION ########
+
+
+
+
+
+
+
+####### NEURAL NETWORK #######
+
+
+
+
+
